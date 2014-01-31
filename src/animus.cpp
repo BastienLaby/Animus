b67cd140060c292dd369b683b9f3306875d22ebd
BastienLaby <bastien.laby@gmail.com>
2014-01-31 20:00:17 +0100

    Some clean

--- src/animus.cpp
+++ src/animus.cpp
@@ -24,7 +24,6 @@
 #include "glm/gtc/matrix_transform.hpp" // glm::translate, glm::rotate, glm::scale, glm::perspective
 #include "glm/gtc/type_ptr.hpp" // glm::value_ptr
 
-#include "CameraTools.hpp"
 #include "ShaderTools.hpp"
 #include "IMGUITools.hpp"
 #include "SoundTools.hpp"
